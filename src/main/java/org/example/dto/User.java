9ac5a7351ab021c3120352d2fb02e6d8f3d9e0ec
Phong Ho Hoai <phonghh@unit.com.vn>
2024-06-25 20:50:18 +0700

    init test momo

---
+++ src/main/java/org/example/dto/User.java
@@ -0,0 +1,29 @@
+package org.example.dto;
+
+public class User {
+
+    private String username;
+
+    private long balance;
+
+    public User(String username, long balance) {
+        this.username = username;
+        this.balance = balance;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+}
